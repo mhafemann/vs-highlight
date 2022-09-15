a1fda960dc682463bb7f40b6c064f2f6d30041e4
Mark Hafemann <markhafemann1@gmail.com>
2022-09-15 11:25:16 -0500

    reformatted comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,59 +1,56 @@
 import { ExtensionContext, Disposable } from 'vscode';
 import { commands } from 'vscode';
 import {
-   getTheme,
-   showQuickPick,
-   themes,
-   insertText,
-   getSelection,
-   highlight,
-   message,
+  getTheme,
+  showQuickPick,
+  themes,
+  insertText,
+  getSelection,
+  highlight,
+  message,
 } from './lib';
 
-// prettier-ignore
 export function activate(context: ExtensionContext) {
+  // insert the given text into the current editor.
+  const insertStyles: Disposable = commands.registerCommand(
+    'vs-highlight.insertThemeStyles',
+    async () => {
+      const selection = await showQuickPick(themes);
 
-   // Insert the given text into the current editor.
-   const insertStyles: Disposable = commands.registerCommand(
-      'vs-highlight.insertThemeStyles',
-      async () => {
-        
-         const selection = await showQuickPick(themes);
-         
-         if (selection) {
-            const css = await getTheme(selection);
-            if (css) {
-               insertText(css);
-            }
-         }
+      if (selection) {
+        const css = await getTheme(selection);
+        if (css) {
+          insertText(css);
+        }
       }
-   );
+    }
+  );
 
-   // Highlight the current selection.
-   const highlightSelection: Disposable = commands.registerCommand(
+  // highlight the current selection.
+  const highlightSelection: Disposable =
+    commands.registerCommand(
       'vs-highlight.highlightSelection',
       async () => {
-         const text = await getSelection();
+        const text = await getSelection();
 
-         if (text) {
-            const highlighted = await highlight(
-               text, 
-               {includePreTag: true}
-            );
+        if (text) {
+          const highlighted = await highlight(text, {
+            includePreTag: true,
+          });
 
-            if (highlighted) {
-               insertText(highlighted);
-            }
-            
-            message.info('Selection highlighted.');
-         }
+          if (highlighted) {
+            insertText(highlighted);
+          }
+
+          message.info('Selection highlighted.');
+        }
       }
-   );
+    );
 
-   // Register the commands.
-   context.subscriptions.push(insertStyles);
-   context.subscriptions.push(highlightSelection);
+  // register the commands.
+  context.subscriptions.push(insertStyles);
+  context.subscriptions.push(highlightSelection);
 }
 
-// method called when extension is deactivated
+// method called when extension is deactivated.
 export function deactivate() {}
