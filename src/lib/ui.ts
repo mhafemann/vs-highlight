b708e2c726fef67bc588f3bee1e367bf9c25d931
Mark Hafemann <markhafemann1@gmail.com>
2022-09-22 15:54:29 -0500

    removed showConfirmation function

--- src/lib/ui.ts
+++ src/lib/ui.ts
@@ -1,11 +1,10 @@
 import { window } from 'vscode';
-import { message } from './message';
 
 /**
  * Show a quick pick menu and return the user selection.
  * @param items string[] The items to show in the quick pick menu.
  * @param title string The title of the quick pick menu.
- * @returns {Promise<string | undefined>} Promise<string | null> The selected item or null.
+ * @returns {Promise<string | undefined>} Promise<string | undefined> The selected item or undefined.
  */ // prettier-ignore
 export const showQuickPick = async (
    items: string[],
@@ -19,23 +18,4 @@ export const showQuickPick = async (
       });
 
    return result;
-};
-
-// prettier-ignore
-export const showConfirmation = async (
-	title: string,
-	prompt: string,
-	initialValue: string
-) => {
-	
-const result = await window.showInputBox({
-	title: 'Confirm selection',
-	prompt: prompt,
-});
-
-if (result === 'y') {
-	console.log('result is y');
-} else {
-	console.log('result is not y');
-}
 };
