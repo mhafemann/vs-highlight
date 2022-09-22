462e4914d4c133561582c10842bd427768640499
Mark Hafemann <markhafemann1@gmail.com>
2022-09-22 14:25:33 -0500

    wip - function for generating files in workspace.

---
+++ src/lib/filesystem.ts
@@ -0,0 +1,8 @@
+import { workspace } from 'vscode';
+
+const root = workspace.folders;
+
+export const createFile = async (path, content) => {
+   console.log(root);
+   return workspace.fs.writeFile(path, new TextEncoder().encode(content));
+};
